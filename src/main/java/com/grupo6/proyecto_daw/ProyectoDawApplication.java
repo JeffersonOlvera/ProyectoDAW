55265fd3d5dbec646b4eba9c72d76a0f154591ab
jeffersonolvera <olverasuarezjeff@gmail.com>
2024-11-02 17:55:11 -0500

    First Commit

---
+++ src/main/java/com/grupo6/proyecto_daw/ProyectoDawApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.proyecto_daw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawApplication.class, args);
+	}
+
+}
